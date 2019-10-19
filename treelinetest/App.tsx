b1b9eed2de70c4f27f464a4a4b0846652f821a73
Alex Rodriguez <rodriguez.alexrod.alex@gmail.com>
2019-10-19 13:51:10 -0500

    Initial commit test branch

--- treelinetest/App.tsx
+++ treelinetest/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>This is a test</Text>
+      <Text>EPIC GAMING MOMENT</Text>
     </View>
   );
 }
