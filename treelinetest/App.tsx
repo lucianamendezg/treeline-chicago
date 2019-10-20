66f803e208eca26e2bef9f45a3d9fb3d383261ef
Luciana Mendez <luciana.mendezg@gmail.com>
2019-10-19 19:52:34 -0500

    Inital Map Commit

--- treelinetest/App.tsx
+++ treelinetest/App.tsx
@@ -6,19 +6,18 @@ import { styles } from "./css/css";
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import { home_screen } from './pages/HomeScreen';
-import {ProfileScreen} from './pages/ProfileScreen'
+import {ProfileScreen} from './pages/ProfileScreen';
+import {MapScreen} from './pages/MapScreen';
 
 
 
 const MainNavigator = createStackNavigator({
   Home: {screen: home_screen},
   Profile: {screen: ProfileScreen},
+  Map: {screen: MapScreen}
 });
 
 const App = createAppContainer(MainNavigator);
 
 
 export default App;
-
-
-
---
+++ treelinetest/pages/MapScreen.tsx
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {StyleSheet,View} from "react-native";
+import MapboxGL from "react-native-mapbox-gl/maps";
+//import {styles} from "./css/css";
+//This is the token we own in the mapbox api
+
+MapboxGL.setAccessToken("<pk.eyJ1Ijoiam9obmxpa2VhYmF3c2UiLCJhIjoiY2sxeTQwaTJxMGpnMTNicGk4YjdnYW1qZSJ9.Q0wodY6Nvt2iVxtWwbyQLQ>");
+
+export default class App extends Component{
+  componentDidMount() {
+  MapboxGL.setTelemetryEnabled(false);
+}
+
+render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
+});
