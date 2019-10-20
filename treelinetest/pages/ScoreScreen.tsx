b2996a678ca926327a499e1cd6187bc342143f3e
John <jyenterbriars@gmail.com>
2019-10-20 01:34:58 -0500

    Initial commit of score func

---
+++ treelinetest/pages/ScoreScreen.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {StyleSheet,View} from "react-native";
+
+
+export class ScoreScreen extends Component{
+
+    render(){
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
