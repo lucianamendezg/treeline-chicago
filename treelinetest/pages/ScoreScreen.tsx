f7b651274670a1242687c17c6cc8e2f2202f038e
John <jyenterbriars@gmail.com>
2019-10-20 03:44:14 -0500

    Saved score working

--- treelinetest/pages/ScoreScreen.tsx
+++ treelinetest/pages/ScoreScreen.tsx
@@ -1,14 +1,27 @@
 import React, {Component} from "react";
-import {StyleSheet,View} from "react-native";
+import {StyleSheet,View, Text, AsyncStorage, Button} from "react-native";
+import customData from './score/score.json';
+import { number } from "prop-types";
 
 
 export class ScoreScreen extends Component{
 
+    static current_score = 0;
+
+
+    increaseScore(increase){
+        ScoreScreen.current_score += increase;
+    }
+
+    decreaseScore(decrease){
+        ScoreScreen.current_score -= decrease;
+    }
+
     render(){
         return (
-            <View>
-                
-            </View>
+        <View>
+            <Text>Current Score: {ScoreScreen.current_score}</Text>
+        </View>
         );
     }
 }
